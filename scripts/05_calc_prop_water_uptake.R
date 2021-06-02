cf99ed523865dd18a4197641db65c783260a1a93
Martin Holdrege <martinholdrege@gmail.com>
2021-06-02 15:51:35 -0600

    started to filter out drifted values

--- scripts/05_calc_prop_water_uptake.R
+++ scripts/05_calc_prop_water_uptake.R
@@ -15,23 +15,96 @@ library(tidyverse)
 
 # read in data ------------------------------------------------------------
 
-raw1 <- read_csv("data-processed/hw_combined_cc_output.csv")
+# compiled chemcorrect output
+cc1 <- read_csv("data-processed/hw_combined_cc_output.csv")
+
+# compiled uncorrected data (for determining which samples are 'good')
+raw1 <- read_csv("data-processed/hw_combined_picarro_output.csv") %>% 
+  janitor::clean_names()
 
 # parsing -----------------------------------------------------------------
 
-raw2 <- raw1 %>% 
+
+
+cc2 <- cc1 %>% 
   # 2 sampling events
   mutate(sample_event = ifelse(lubridate::month(date_inject) == 5,
-                               1 , 2))
+                               1 , 2),
+         run = str_replace(run, "_\\d$", ""))
 
-raw3 <- raw2 %>% 
-  filter(date != "2020-07-12")
+
+
+# categorizing good/bad samples ------------------------------------------
+
+# calculate slope
+calc_slope <- function(y) {
+  
+  if(sum(!is.na(y)) < 2) {
+    return(NA_real_)
+  }
+  
+  x <- 1:length(y)
+  
+  mod <- lm(y ~ x)
+  mod$coefficients[2]
+}
+
+
+
+# creating a lookup vector, to convert line number into
+# the unique sampling occasion. I.e. this is the nth vial, to be sampled
+n_lines <- 694
+vial_inj_lookup <- vector(mode = "numeric", length = n_lines)
+# first vial is measured 10 times
+vial_inj_lookup[1:10] <- 1
+# remaining samples are measured 6 times
+vial_inj_lookup[11:n_lines] <- 0:(n_lines-11) %/% 6 + 2
+
+names(vial_inj_lookup) <- 1:n_lines
+
+raw_means1 <- raw1 %>% 
+  # vial number (i.e. consecutive numbering that vials were actually measured,
+  # so have an identifier to tell what the 'previous' vial was, to measure
+  # memory of hot samples)
+  mutate(vial_nr = vial_inj_lookup[line]) %>% 
+  # only keep last 3 samples
+  filter(inj_nr > 3) %>% 
+  group_by(run, port, vial_nr, identifier_1, identifier_2) %>% 
+  summarize(slope = calc_slope(y = d_d_h_mean),
+            d_d_h_sd = sd(d_d_h_mean, na.rm = TRUE),
+            d_d_h_mean = mean(d_d_h_mean, na.rm = TRUE)) %>% 
+  arrange(run, vial_nr) %>% 
+  group_by(run) %>% 
+  # difference between mean of this sample and the previous sample
+  mutate(d_h_diff = c(NA, diff(d_d_h_mean)))
+
+# samples are bad if the previous sample was very hot and there
+# is a lot of difference between injections
+raw_means2 <- raw_means1 %>%
+  mutate(is_bad = ifelse(d_d_h_sd > 10 & slope < 0 & d_h_diff < -200,
+                         TRUE, FALSE))
+
+sum(raw_means2$is_bad, na.rm = TRUE)
+
+
+# * removing bad values ---------------------------------------------------
+
+cc3 <- raw_means2 %>% 
+  filter(!(identifier_2 %in% c("Dummy", "Standard", "Tap"))) %>% 
+  select(run, identifier_1, is_bad) %>% 
+  mutate(identifier_1 = as.numeric(identifier_1)) %>% 
+  right_join(cc2, by = c("run", "identifier_1" = "vial_num")) %>% 
+  # excluding samples affected by drift from previous hot sample
+  filter(!is_bad)
+  
+
+
 
 # checking control data ---------------------------------------------------
 
 # data was collected prior to event 1 to make sure no tracer was detected
 
-control <- raw2 %>% 
+control <- cc3 %>% 
   filter(date == "2020-07-12")
 
 control %>%
@@ -40,4 +113,7 @@ control %>%
 
 hist(control$raw_2h_mean) # some high (contaminated?) values
 
+
+cc4 <- cc3 %>% 
+  filter(date != "2020-07-12")
 
